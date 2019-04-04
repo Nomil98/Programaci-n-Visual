49c3d3ac8ba2151244d98fd18c8b3ae892f80d69
Nomil98 <47511927+Nomil98@users.noreply.github.com>
2019-04-03 21:32:39 -0600

    Programa que aplica la Coladera de Eratóstenes.
    presenta un problema que sobre pasa el límite de la matriz.

---
+++ Coladera.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coladera_Eratostenes
+{
+    class Coladera_Eratostenes
+    {
+        private int[] _Vector;
+
+        public Coladera_Eratostenes() //Se le asigna el tamaño al vector.
+        {
+            _Vector = new int[1000];
+        }
+
+        public void Llenar() //Le asignamos a todo el vector el valor de uno.
+        {
+            for (int i = 1; i < _Vector.Length; i++)
+            {
+                _Vector[i] = 1;
+            }
+
+        }
+
+        public void Coladera()///Aquí se hace la asignación de ceros a los números no primos y 
+                              //se quedan sólo los subíndices con valor a 1 que serán los primos. 
+        {
+            for(int i=2; i<_Vector.Length/2; i++)
+            {
+                if(_Vector[i]==1)
+                {
+                    for (int j = i + i; i < _Vector.Length; j += i)
+                        if (j % i == 0)
+                            _Vector[j] = 0;
+                }
+            }
+        }
+
+        public string Mostrar()//Se van a mostrar sólo los subíndices  con valor de 1.
+        {
+            string primos= "";
+            for(int i =1; i<_Vector.Length; i++ )
+            {
+                if (_Vector[i] == 1)
+                    primos = primos + i.ToString() + Environment.NewLine;
+
+                
+            }
+            return primos;  
+        }
+
+
+
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Vectores_SegundaParcial
+namespace Coladera_Eratostenes
 {
     public partial class Form1 : Form
     {
@@ -17,46 +17,18 @@ namespace Vectores_SegundaParcial
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        Coladera_Eratostenes vector = new Coladera_Eratostenes();
+        private void button1_Click(object sender, EventArgs e)
         {
-
-        }
-        miVector vector;
-        private void BtnCrear_Click(object sender, EventArgs e)
-        {
-            vector = new miVector(Convert.ToInt32(TxtTam.Text));
-
-        }
-        
-        private void BtnLlenar_Click(object sender, EventArgs e)
-        {
-            vector.Llenar(Convert.ToInt32(TxtVal.Text));
+            vector.Llenar();
+            vector.Coladera();
 
 
         }
 
         private void BtnMostrar_Click(object sender, EventArgs e)
         {
-            TxtVec.Text = Convert.ToString(vector.Mostrar());
-
-        }
-
-        private void BtnInvertir_Click(object sender, EventArgs e)
-        {
-            vector.GirarArriba();
-            TxtVec.Text = vector.Mostrar();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            vector.GirarAbajo();
-            TxtVec.Text = vector.Mostrar();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            vector.Invertir();
-            TxtVec.Text = vector.Mostrar();
+            listBox1.Text = vector.Mostrar();
         }
     }
 }
